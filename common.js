39da2728bbcbe9ccbe64955ad8ac70d2e69f08bb
Avantgarde95 <mathematicianscott@gmail.com>
2022-04-24 13:15:48 +0900

    [feat] Put common footer
    * Put common footer to the apps
      * It shows the apps' links

---
+++ common.js
@@ -0,0 +1,32 @@
+const links = [
+    ['Cat 1', './cat1.html'],
+    ['Cat 2', './cat2.html'],
+    ['Cat 3', './cat3.html'],
+    ['Code', 'https://github.com/Avantgarde95/cat']
+];
+
+const Footer = document.createElement('div');
+
+Footer.style.marginTop = '0.5rem';
+
+links.forEach(([label, url], index) => {
+    const Link = document.createElement('a');
+
+    Link.href = url;
+    Link.innerHTML = label;
+    Link.style.color = 'inherit';
+
+    if (index !== 0) {
+        Link.style.marginLeft = '0.5rem';
+    }
+
+    Footer.appendChild(Link);
+});
+
+const Target = document.querySelector('.app');
+
+if (Target !== null) {
+    Target.appendChild(Footer);
+} else {
+    document.body.appendChild(Footer);
+}
